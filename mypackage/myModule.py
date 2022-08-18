8fc2e09c108b05a0647f48c5346ab00652c72c2d
lightfixercode <iduwaoma@protonmail.com>
2022-08-18 21:31:13 +0100

    First commit

---
+++ mypackage/myModule.py
@@ -0,0 +1,30 @@
+def top_n(items, n):
+	"""Return the top n items in an array, in descending order.
+	
+	Args:
+		items (array): list or array-like object containing numerical values.
+		n (int): number of top items to return.
+	
+	Returns:
+		array: top n items, in descending order.
+	
+	Examples:
+		>>> top_n([8, 3, 2, 7, 4], 3)
+		[8, 7, 3]
+	"""
+
+	for i in range(n):  # Keep sorting until we have the top n items.
+		for j in range(len(items)-1-i):
+
+			if items[j] > items[j+1]:  # if this item is bigger than the next item..
+				items[j], items[j+1] = items[j+1], items[j]  # swap the two!
+				
+	# Get the last two items
+	top_n = items[-n:]
+
+	# Return in descending order
+	return top_n[::-1]
+
+# check if function works
+a=top_n([8, 3, 2, 7, 4], 3)
+print(a)
---
+++ readme.md
@@ -0,0 +1,12 @@
+# mypackage
+This library was published as an example of how to publish your own python  package.
+
+
+## Building this package locally
+`python setup.py sdist`
+
+
+## Installing this package from GitHub
+`pip install git + https://github.com/lightfixercode/mypackage`
+
+## Updating this package from GitHub
